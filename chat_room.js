af024c92bd2026a2be0684733e58f3bf17d10b4e
Aayaana <119086392+aayaanachauhan@users.noreply.github.com>
2023-05-11 15:33:01 -0700

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAXn7swcC7VVOLgcZg8XNUwDSO8igcrcsA",
+    authDomain: "lets-chat-app-f5d9b.firebaseapp.com",
+    databaseURL: "https://lets-chat-app-f5d9b-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-app-f5d9b",
+    storageBucket: "lets-chat-app-f5d9b.appspot.com",
+    messagingSenderId: "348950623489",
+    appId: "1:348950623489:web:45f5147654816850edd700"
+  };
+  
+ 
